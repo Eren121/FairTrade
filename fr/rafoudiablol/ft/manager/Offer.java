4726fdd78140075af531d9215ea031197dfabda6
Eren121 <frantze.raphael@gmail.com>
2018-09-23 00:09:38 +0200

    New classes for trade managements: Offer, Trade (no logic, fields only)

---
+++ fr/rafoudiablol/ft/manager/Offer.java
@@ -0,0 +1,24 @@
+package fr.rafoudiablol.ft.manager;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class Offer {
+
+    protected Player p; // The player that offers what following
+    protected boolean confirmed; // If the player has confirmed or not
+    protected ItemStack[] items; // Items that the player want to give
+
+    public Offer(Player p) {
+
+        this.p = p;
+    }
+
+    public boolean confirm() {
+        return confirmed;
+    }
+
+    public void toggle() {
+        confirmed = !confirmed;
+    }
+}
---
+++ fr/rafoudiablol/ft/manager/Trade.java
@@ -0,0 +1,10 @@
+package fr.rafoudiablol.ft.manager;
+
+public class Trade {
+
+    protected Offer offers[] = new Offer[2];
+
+    public Trade() {
+
+    }
+}
