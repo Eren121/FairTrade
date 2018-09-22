5470839cf3976e10c1f737886c7234530ea93aa9
Eren121 <frantze.raphael@gmail.com>
2018-09-23 00:01:05 +0200

    ItemStaxs add varargs and set metaCall() to setMeta()

--- fr/rafoudiablol/ft/utils/ItemStaxs.java
+++ fr/rafoudiablol/ft/utils/ItemStaxs.java
@@ -11,21 +11,21 @@ public class ItemStaxs {
 
     public static void rename(ItemStack i, String display)
     {
-        metaCall(i, meta -> meta.setDisplayName(display));
+        setMeta(i, meta -> meta.setDisplayName(display));
     }
 
     public static void brief(ItemStack i, String... des)
     {
-        metaCall(i, meta -> meta.setLore(asList(des)));
+        setMeta(i, meta -> meta.setLore(asList(des)));
     }
 
-    public static void renameAndBrief(ItemStack i, String s1, String s2)
+    public static void renameAndBrief(ItemStack i, String s1, String... s2)
     {
         rename(i, s1);
         brief(i, s2);
     }
 
-    public static void metaCall(ItemStack i, Consumer<ItemMeta> f)
+    public static void setMeta(ItemStack i, Consumer<ItemMeta> f)
     {
         ItemMeta meta = i.getItemMeta();
         f.accept(meta);
