5780c4b951ca07973c1ab1330685c7cfa81f4d81
Eren121 <frantze.raphael@gmail.com>
2018-09-23 15:14:13 +0200

    Create new listener class for tracking requests: RequestTracker

---
+++ fr/rafoudiablol/ft/listeners/RequestTracker.java
@@ -0,0 +1,35 @@
+package fr.rafoudiablol.ft.listeners;
+
+import fr.rafoudiablol.ft.config.EnumI18n;
+import fr.rafoudiablol.ft.events.AbortTransactionEvent;
+import fr.rafoudiablol.ft.events.AcceptTransactionEvent;
+import fr.rafoudiablol.ft.events.RequestTransactionEvent;
+import fr.rafoudiablol.ft.main.FairTrade;
+import fr.rafoudiablol.ft.manager.ITransactionLink;
+import fr.rafoudiablol.ft.manager.PlayerStatus;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class RequestTracker implements Listener {
+
+    private HashMap<Player, Player> targetToRequester = new HashMap<>();
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void event(RequestTransactionEvent e) {
+
+        targetToRequester.put(e.getOther(), e.getPlayer());
+        FairTrade.getFt().sendMessage(EnumI18n.REQUEST.localize(e.getPlayer()), e.getOther());
+    }
+
+    @EventHandler
+    public void event(AcceptTransactionEvent e) {
+
+        e.setTarget(targetToRequester.remove(e.getPlayer(1)));
+    }
+}
