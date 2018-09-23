c4c8d1facef3e498fff5a5a8afaea862e510af7a
BuildTools <unconfigured@null.spigotmc.org>
2018-09-23 23:12:43 +0200

    fix: "Distance too high" message sent to wrong player when requested

--- fr/rafoudiablol/ft/listeners/RequiredDistance.java
+++ fr/rafoudiablol/ft/listeners/RequiredDistance.java
@@ -14,22 +14,25 @@ public class RequiredDistance implements Listener {
 
     @EventHandler
     public void event(RequestTransactionEvent e) {
-        doEvent(e);
+        doEvent(e, 0);
     }
 
     @EventHandler
     public void event(InitiateTransactionEvent e) {
-        doEvent(e);
+        doEvent(e, 0, 1);
     }
 
-    private void doEvent(AbstractTransactionEvent e) {
+    private void doEvent(AbstractTransactionEvent e, int... ids) {
 
         Player p1 = e.getTrade().getOffer(0).getPlayer();
         Player p2 = e.getTrade().getOffer(1).getPlayer();
         String reason = getReason(p1, p2);
 
         if (reason != null) {
-            FairTrade.getFt().sendMessage(reason, p2);
+
+            for(int id : ids)
+                FairTrade.getFt().sendMessage(reason, e.getTrade().getOffer(id).getPlayer());
+
             ((Cancellable)e).setCancelled(true);
         }
     }
