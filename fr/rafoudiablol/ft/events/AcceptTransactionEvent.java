a11fe92acbd84ae0a4e126bd5404b05332185763
Eren121 <frantze.raphael@gmail.com>
2018-09-23 15:06:42 +0200

    Create AcceptTransactionEvent class

---
+++ fr/rafoudiablol/ft/events/AcceptTransactionEvent.java
@@ -0,0 +1,31 @@
+package fr.rafoudiablol.ft.events;
+
+import fr.rafoudiablol.ft.manager.Trade;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Fire when a player type an /accept command
+ */
+public class AcceptTransactionEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+    @Override public HandlerList getHandlers() {
+        return handlers;
+    }
+    @SuppressWarnings("unused") public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    protected Player players[0];
+
+    public AcceptTransactionEvent(Player p1, Player p2) {
+        players = new Player[] {p1, p2};
+    }
+
+    public Player getPlayer(int i) {
+        return players[i];
+    }
+}
